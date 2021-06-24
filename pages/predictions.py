cd2baf4d757ef6a93b848bd71ec34672c5b9b2de
RowenWitt <rew4200@protonmail.com>
2021-06-23 23:40:09 -0700

    got the button to work

--- pages/predictions.py
+++ pages/predictions.py
@@ -4,7 +4,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_daq as daq
 from .newpage import sub_category_list
 # Imports from this application
@@ -27,15 +27,15 @@ with open('ToSite/tokenizer.pickle', 'rb') as handle:
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-@app.callback(
-    Output('prediction-content', 'children'),
-    [Input('usd_goal', 'value'), 
-    Input('category', 'value'),
-    Input('timeline', 'value'),
-    Input('sub_category', 'value'),
-    Input('text', 'value'),
-    ],
-)
+# @app.callback(
+#     Output('prediction-content', 'children'),
+#     [Input('usd_goal', 'value'), 
+#     Input('category', 'value'),
+#     Input('timeline', 'value'),
+#     Input('sub_category', 'value'),
+#     Input('text', 'value'),
+#     ],
+# )
 def predict(usd_goal,category,timeline,sub_category,text):
     df = pd.DataFrame(
         columns=[0,
@@ -51,19 +51,27 @@ def predict(usd_goal,category,timeline,sub_category,text):
     seq = tokenizer.texts_to_sequences(df[3])
     # seq2 = [i for s in seq for i in s]
     padded_sequence = sequence.pad_sequences(seq, maxlen)
+    # Convert text to df columns (inelegant, but effective)
     for j in range(5, 48):
         f = j-5
         df[j] = [padded_sequence[0][f]]
+    # Drop unencoded text column
     df.drop(columns=[3], inplace=True)
+    # Convert df to numpy array, for further conversion to tensor
     arr = df.to_numpy()
-    print(arr)
+    print(arr)  # DEV, delete before prod
+    # Reshape array into 
     tarr = arr.reshape(1, 47, 1)
     tarr = np.asarray(tarr).astype('float32')
     model = keras.models.load_model('ToSite/models')
     print(model.summary())
     print(arr.shape)
     y_pred_proba = round((model.predict(tarr)[0][0] * 100), 2)
-    y_pred = model.predict_classes(tarr)[0][0]
+    if(y_pred_proba) >= 0.5:
+        y_pred = 1
+    else:
+        y_pred = 0
+
     return '{}, {}'.format(y_pred, y_pred_proba)
 
 
@@ -155,33 +163,42 @@ column1 = dbc.Col(
             className='mb-5',
         ), 
         
-        # dcc.Markdown(
-        #     """
-        
-        #     ## Feature Explination
-
-        #     The features on the right are the 7 major contributors to the machine 
-        #     learning model. 
-
-        #     ##### Unique ID : 
-        #     ToDo
-        #     ##### Since Created : 
-        #     ToDo
-        #     ##### Since Launched : 
-        #     ToDo
-        #     ##### Since Changed : 
-        #     ToDo
-        #     ##### USD Pledged : 
-        #     ToDo
-        #     ##### Text : 
-        #     ToDo
-        #     ##### State : 
-        #     ToDo
-        #     """
-        # ),
+        dbc.Button("Predict Kickstarter Success", id="example-button", color='primary',
+                   className="mr-2"),
+        html.Div(id='container-button-timestamp'),
+        html.Span(id="example-output",
+                  style={"vertical-align": "middle"}),
     ],
     md=6,
 )
+
+@app.callback(
+    Output("prediction-content",
+           "children"), [Input("example-button", 'n_clicks')],
+    [
+        State('usd_goal', 'value'),
+        State('category', 'value'),
+        State('timeline', 'value'), 
+        State('sub_category', 'value'),
+        State('text', 'value'),
+        
+
+#     Output('prediction-content', 'children'),
+#     [Input('usd_goal', 'value'), 
+#     Input('category', 'value'),
+#     Input('timeline', 'value'),
+#     Input('sub_category', 'value'),
+#     Input('text', 'value'),
+    ]
+)
+def on_button_click(n, timeline,usd_goal,category,text,sub_category):
+    '''
+    on_button_click function passes information from the model on clicl
+    '''
+    if n is None:
+        return "Please fill in the Kickstarter Model"
+    else:
+        return predict(timeline,usd_goal,category,text,sub_category)
 
 column2 = dbc.Col(
     className='mb-40'
